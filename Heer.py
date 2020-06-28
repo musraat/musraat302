fd3d7be68178d17ecb1754b823ccf170d873a334
musraat <67539656+musraat@users.noreply.github.com>
2020-06-28 15:20:17 +0500

    Update and rename zeroboy.py to Heer.py

--- Heer.py
+++ Heer.py
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
+|__(~)    •||•THE - MUSRAAT -OFFICAL------•◈•
+|__\~~) •||•HEER - KANWAL---------------•◈•
 |__(-----\  •◈•------BLACK-TIGER--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mMUSRAAT
+\033[1;96mInstagram \033[1;93m: \033[1;NAI HA
+\033[1;96mFacebook  \033[1;93m: \033[1; 100015137282621
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/musraat
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mHEER KANWAL
+\033[1;96mInstagram \033[1;93m: \033[1;92mNai ha
+\033[1;96mFacebook  \033[1;93m: \033[1;92m 100015137282621
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/musraat/jahan
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "Heer"
+CorrectPassword = "Kanwal"
 
 loop = 'true'
 while (loop == 'true'):
